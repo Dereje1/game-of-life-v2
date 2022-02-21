69a3d6b220c7d6d1ba8d4a96d0456191a36068c3
Dereje Getahun <dgetahun@Derejes-MacBook-Pro.local>
2022-02-21 08:32:35 -0500

    add grid and clickable cells

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+const GRANULARITY = 20;
 class Home extends Component {
 
     constructor(props) {
@@ -12,28 +13,108 @@ class Home extends Component {
         this.loadCanvas()
     }
 
-    loadCanvas = () => {
-        // loads canvas once on game mount
+    updateCell = () => {
+        const { canvasWidth, canvasHeight } = this.state;
+        const context = this.canvasContext
+        const activeCells = Object.keys(this.state).filter(c => this.state[c].isActive)
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+        activeCells.forEach(a => {
+            const [x, y] = a.split('-');
+            context.fillStyle = "green";
+            context.fillRect(Number(x), Number(y), GRANULARITY, GRANULARITY)
+        })
+        this.refreshGrid();
+    }
+    getClick = (event) => {
+        const modelKeys = Object.keys(this.state);
         const Canvas = this.Canvas.current;
-        const { innerWidth, innerHeight } = window
-        console.log(Canvas)
+        const rect = Canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        let modelX = modelKeys.map(m => Number(m.split('-')[0]))
+        let modelY = modelKeys.map(m => Number(m.split('-')[1]))
+        modelX = Array.from(new Set(modelX))
+        modelY = Array.from(new Set(modelY))
+        let xRect = 0
+        let yRect = 0
+        for (let idx = 0; idx <= modelX.length; idx++) {
+            if (modelX[idx] > x) {
+                xRect = modelX[idx - 1]
+                break
+            }
+        }
+        for (let idx = 0; idx <= modelX.length; idx++) {
+            if (modelY[idx] > y) {
+                yRect = modelY[idx - 1]
+                break
+            }
+        }
+        const key = `${xRect}-${yRect}`;
+
+        this.setState({
+            [key]: {
+                isActive: !this.state[key].isActive
+            }
+        }, this.updateCell)
+
+    }
+
+    setGrid = () => {
+        const { canvasWidth, canvasHeight } = this.state;
+        for (let x = 0; x <= canvasWidth; x += GRANULARITY) {
+            for (let y = 0; y <= canvasHeight; y += GRANULARITY) {
+                this.setState({
+                    [`${x}-${y}`]: {
+                        isActive: false
+                    }
+                })
+            }
+        }
+        this.refreshGrid()
+    }
+
+    refreshGrid = () => {
+        const context = this.canvasContext
+        const { canvasWidth, canvasHeight } = this.state;
+        for (let x = 0; x <= canvasWidth; x += GRANULARITY) {
+            context.beginPath();
+            context.moveTo(x, 0);
+            context.lineTo(x, canvasHeight);
+            context.strokeStyle = "white";
+            context.stroke();
+        }
+        for (let y = 0; y <= canvasHeight; y += GRANULARITY) {
+            context.beginPath();
+            context.moveTo(0, y);
+            context.lineTo(canvasWidth, y);
+            context.strokeStyle = "white";
+            context.stroke();
+        }
+    }
+    loadCanvas = () => {
+        const Canvas = this.Canvas.current;
+        const { innerWidth, innerHeight } = window;
+        const cellWidth = Math.floor((innerWidth * 0.8) / GRANULARITY);
+        const cellHeight = Math.floor((innerHeight * 0.8) / GRANULARITY);
+        const canvasWidth = cellWidth * GRANULARITY;
+        const canvasHeight = cellHeight * GRANULARITY
+
         if (Canvas) {
             Canvas.focus();
             Canvas.style.backgroundColor = 'black';
             this.canvasContext = Canvas.getContext('2d');
             this.Canvas.current.focus();
             this.setState({
-                canvasWidth: innerWidth * 0.8,
-                canvasHeight: innerHeight * 0.8,
-                canvasTop: innerHeight * 0.1,
-                canvasLeft: innerWidth * 0.1
-            });
+                canvasWidth,
+                canvasHeight,
+                canvasTop: (innerHeight - canvasHeight) / 2,
+                canvasLeft: (innerWidth - canvasWidth) / 2
+            }, this.setGrid);
         }
     }
 
     render() {
         const { canvasWidth, canvasHeight, canvasTop, canvasLeft } = this.state;
-        console.log()
         return (
             <div style={{ paddingTop: canvasTop, paddingLeft: canvasLeft }}>
                 <canvas
@@ -41,8 +122,7 @@ class Home extends Component {
                     width={canvasWidth}
                     height={canvasHeight}
                     tabIndex="0"
-                    onKeyDown={e => ({})}
-                    onKeyUp={e => ({})}
+                    onClick={this.getClick}
                 />
             </div>
         )
